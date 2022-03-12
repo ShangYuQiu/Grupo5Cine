ae6551ab5a6d2283a87b2328ebf77e9f375801cc
ShangYuQiu <101433056+ShangYuQiu@users.noreply.github.com>
2022-03-12 23:23:12 +0100

    Update Controller.java

--- IS2/src/control/Controller.java
+++ IS2/src/control/Controller.java
@@ -47,7 +47,7 @@ public class Controller {
 		boolean refresh = true;
 		
 		while ( refresh) {
-			
+			//mostrar menu
 			menu();
 			System.out.println("Introduce opcion");
 			int i = scanner.nextInt();
